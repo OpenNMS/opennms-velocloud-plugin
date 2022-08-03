2b968bcd6962aa817e47eecba38c6d1c82b98d3e
aramosvizcarra <aramos-vizcarra@nanthealth.com>
2022-08-03 09:31:19 -0400

    Update plugin/src/main/java/org/opennms/velocloud/ApiClient.java
    Co-authored-by: Dustin Frisch <dustin@opennms.com>

--- plugin/src/main/java/org/opennms/velocloud/ApiClient.java
+++ plugin/src/main/java/org/opennms/velocloud/ApiClient.java
@@ -12,7 +12,7 @@ import org.opennms.velocloud.model.Topology;
 public class ApiClient extends org.opennms.velocloud.client.handler.ApiClient {
 
     public static final String AUTHORIZATION = "Authorization";
-    public static final String HEADER = "header";
+    public static final String AUTH_HEADER_LOCATION = "header";
     public static final String AUTH_HEADER _PREFIX = "Token";
 
     public ApiClient(final String url, final String params, final String authType) {
