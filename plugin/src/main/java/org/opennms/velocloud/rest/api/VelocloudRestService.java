e6b646ac9c186d4aff0eb9d0096e26b7cc5e6a2f
aramosvizcarra <aramosvizcarra@gmail.com>
2022-09-08 14:41:39 -0400

    NMS-14516: initial rest commit

---
+++ plugin/src/main/java/org/opennms/velocloud/rest/api/VelocloudRestService.java
@@ -0,0 +1,22 @@
+package org.opennms.velocloud.rest.api;
+
+import org.opennms.velocloud.client.api.VelocloudApiClient;
+import org.opennms.velocloud.client.api.VelocloudApiException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/velocloud/api")
+public interface VelocloudRestService {
+
+    @GET
+    @Path("/customers/{id}")
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    Response getCustomersForMSPPartnerConnections(@PathParam("id") String id) throws VelocloudApiException;
+
+    VelocloudApiClient getClient(final String url, final String token) throws VelocloudApiException;
+}
---
+++ plugin/src/main/java/org/opennms/velocloud/rest/v1/VelocloudRestServiceV1.java
@@ -0,0 +1,10 @@
+package org.opennms.velocloud.rest.v1;
+
+import org.opennms.velocloud.rest.api.VelocloudRestService;
+
+import javax.ws.rs.Path;
+
+@Path("/v1")
+public interface VelocloudRestServiceV1 extends VelocloudRestService {
+
+}
