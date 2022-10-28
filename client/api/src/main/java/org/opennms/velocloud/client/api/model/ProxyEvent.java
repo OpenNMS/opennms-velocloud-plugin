6593a0f4013a9d7acc6fb0484269feb6794ef457
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2022-10-28 20:50:00 +0200

    NMS-14929: Review changes

--- client/api/src/main/java/org/opennms/velocloud/client/api/model/ProxyEvent.java
+++ client/api/src/main/java/org/opennms/velocloud/client/api/model/ProxyEvent.java
@@ -1,6 +1,35 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.velocloud.client.api.model;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 import org.json.JSONObject;
@@ -19,17 +48,18 @@ public class ProxyEvent {
     private String severity;
 
     private ProxyEvent(final ProxyEvent.Builder builder) {
-        this.detail = builder.detail;
-        this.category = builder.category;
-        this.event = builder.event;
-        this.id = builder.id;
-        this.eventTime = builder.eventTime;
-        this.message = builder.message;
-        this.severity = builder.severity;
-        this.proxyUsername=builder.proxyUsername;
-        this.networkName=builder.networkName;
-        this.enterpriseName=builder.enterpriseName;
-        this.gatewayName=builder.gatewayName;
+        this.detail = Objects.requireNonNull(builder.detail);
+        this.category = Objects.requireNonNull(builder.category);
+        this.event = Objects.requireNonNull(builder.event);
+        this.id = Objects.requireNonNull(builder.id);
+        this.eventTime = Objects.requireNonNull(builder.eventTime);
+        this.message = Objects.requireNonNull(builder.message);
+        this.severity = Objects.requireNonNull(builder.severity);
+
+        this.proxyUsername = builder.proxyUsername;
+        this.networkName = builder.networkName;
+        this.enterpriseName = builder.enterpriseName;
+        this.gatewayName = builder.gatewayName;
     }
 
     public String getDetail() {
